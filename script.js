ada8ea0aa7b1b9e6cc310d9268dfd46f4dc7c663
balajisoundar <balaji@appsmith.com>
2023-07-13 19:49:06 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+((global) => {
+  const propSubscribers = [];
+
+  global.addEventListener("message", (event) => {
+    global.appsmith.props = event.data.props;
+    
+    propSubscribers.forEach((fn) => {
+      if (event.source === global.parent) {
+        fn(event.data.props);
+      }
+    });
+  });
+
+  global.appsmith = {
+    onPropsChange: (fn) => {
+      propSubscribers.push(fn);
+    },
+    upateProps: (obj) => {
+      global.parent.postMessage({
+        type: "update",
+        data: obj
+      });
+    },
+    triggerEvent: (eventName, eventString) => {
+      global.parent.postMessage({
+        type: "event",
+        data: {
+          eventName,
+          eventString
+        }
+      });
+    }
+  };
+})(window);
